12550cfdbf46b909b9c985ceda4e92ae7e4c4aa8
Harry Gakpo <harryhimself7@gmail.com>
2024-05-25 22:28:51 +0000

    configured json and requirements.txt file as well as settings and wsgi file

--- depweb/settings.py
+++ depweb/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-0#99$(crce_kdjyld&5v&_jdrgj1&e-0n7b@nji(pykwi5o*)(
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.vercel.app']
 
 
 # Application definition
